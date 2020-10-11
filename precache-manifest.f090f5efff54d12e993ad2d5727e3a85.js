13ad72d1ead8cea8d1c51de8271860e89b77a74f
aleenprd <71397895+aleenprd@users.noreply.github.com>
2020-10-11 17:13:02 +0200

    Updates

---
+++ precache-manifest.f090f5efff54d12e993ad2d5727e3a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2e135b9b59068536ded2a5f3f8025c",
+    "url": "/chart/index.html"
+  },
+  {
+    "revision": "fa20e84ef7620f551d78",
+    "url": "/chart/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "4d2a7ac49007229b185e",
+    "url": "/chart/static/js/2.9012da12.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/chart/static/js/2.9012da12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa20e84ef7620f551d78",
+    "url": "/chart/static/js/main.30f121db.chunk.js"
+  },
+  {
+    "revision": "0b626bc483e4099b2365",
+    "url": "/chart/static/js/runtime-main.6cf44f8f.js"
+  }
+]);
